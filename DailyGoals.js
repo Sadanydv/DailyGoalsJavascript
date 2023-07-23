8b5ada8fa710778e980ce1afd98c718a61a99252
Sadan Yadav <sadan.yadav.90@gmail.com>
2023-07-23 08:35:33 +0530

    All files Added

---
+++ DailyGoals.js
@@ -0,0 +1,62 @@
+const title = document.getElementById("title");
+const description=document.getElementById("description");
+const form=document.querySelector("form");
+const container=document.querySelector(".container")
+
+const tasks=    localStorage.getItem("tasks")?JSON.parse(localStorage.getItem("tasks")):[];
+MyallTask();
+
+function removeTask(){
+    tasks.forEach(()=>{
+        const rem=document.querySelector(".task");
+        rem.remove();
+    })
+}
+
+function MyallTask(){
+
+   tasks.forEach((value,index)=>{
+    const div=document.createElement("div");
+    div.setAttribute("class","task");
+
+    const innerdiv=document.createElement("div");
+    div.append(innerdiv);
+
+    const p=document.createElement("p");
+    p.innerText=value.title;
+    innerdiv.append(p);
+
+    const span=document.createElement("span");
+    span.innerText=value.description;
+    innerdiv.append(span);
+
+    const btn=document.createElement("button");
+    btn.setAttribute("class","delete-btn")
+
+    btn.addEventListener("click",()=>{
+        removeTask();
+        tasks.splice(index,1);
+        localStorage.setItem("tasks",JSON.stringify(tasks));
+        MyallTask();
+    });
+
+    btn.innerText="-";
+    div.append(btn);
+
+    container.append(div);
+
+   }) 
+
+}
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    removeTask();
+    tasks.push({
+        title:title.value,
+        description:description.value,
+    })
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+    MyallTask();
+
+});
--- multiply.js
+++
@@ -1,6 +0,0 @@
-let a=5;
-let b=10;
-function mult(a,b){
-    return a*b;
-}
-let result=mult(a,b);
